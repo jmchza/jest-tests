7be8171c9070fe695c9b398e0b85fbfa7c961f1b
manuel.chouza <manuel.chouza@gmail.com>
2019-06-05 21:07:26 +1000

    using helpers

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
+const {users} = require('./__tests__/__helpers__/p7')
+
 /**
  * Example problem with existing solution and passing test.
  * See problem 0 in the spec file for the assertion
  * @returns {string}
  */
+
+ const stripPrivateProperties = () => {
+  const remove = ['password', 'token', 'companyId']
+    
+    let subArray = []
+    users.forEach(el => {
+      if(el.name !== 'steve')
+        subArray.push({name: el.name})
+    });
+    //sort
+    const sorting = function(a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA > nameB) {
+        return -1;
+      }
+      if (nameA < nameB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    }
+    subArray.sort(sorting);
+
+    subArray.forEach( function(y) {
+      y['email'] = y.name === 'stan' 
+        ? 'stan@smith.test'
+        : 'sran@smith.test'
+    })
+     
+  return subArray;
+  
+};
 exports.example = () => 'hello world';
 
-exports.stripPrivateProperties = () => {
-    const remove = ['password', 'token']
-    const array1 = [
-        {
-          name: 'stan',
-          email: 'stan@smith.test',
-          password: 'secret',
-          token: '123',
-        },
-        {
-          name: 'fran',
-          email: 'sran@smith.test',
-          password: 'secret',
-          token: '123',
-        },
-      ];
-    let array2 = []
+exports.stripPrivateProperties = stripPrivateProperties
 
- array1.forEach(function (obj) {
-        for (var key in obj) {
-            if (remove.includes(key)) delete obj[key]
-        }
-        
-    })
-    return array1
-    
-};
 exports.excludeByProperty = () => {
-    const array1 = [{ name: 'stan' },
-    { name: 'fran' },
-    { name: 'peter', deleted: true },
-    { name: 'steve' },
-    { name: 'hayley' }]
 
-return array1.filter(x=> (x.deleted ===undefined));
+  const arr = stripPrivateProperties()
+  
+  arr.forEach(function(x) {
+    delete x.email
+  });
+  
+  arr.push({ name: 'peter', deleted: true })
+  arr.push({ name: 'steve' })
+  arr.push({ name: 'hayley' })
+
+return arr.filter(x=> (x.deleted ===undefined));
+
 };
+
 exports.sumDeep = () => {
+
     const array2 = [
         {
           objects: [{ val: 2 }, { val: 1 }, { val: 1 }],
@@ -55,6 +74,7 @@ exports.sumDeep = () => {
 
       return array2.map(u => Object.keys(u).reduce((newObj, key) => true ? { ...newObj, [key]: Object.values(u)[0].reduce(ff, 0)} : null, {}));
 };
+
 exports.applyStatusColor = () => {
     const objRef = {
         red: [404, 400],
